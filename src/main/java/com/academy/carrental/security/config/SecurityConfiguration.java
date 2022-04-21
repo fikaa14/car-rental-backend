18c69eddfd29ac725cb4350b373db08b98c12549
filip <fikaaaa14@gmail.com>
2022-04-22 01:01:14 +0200

    Security configuration added

---
+++ src/main/java/com/academy/carrental/security/config/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.academy.carrental.security.config;
+
+import com.academy.carrental.security.exception.HttpUnauthorizedEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final HttpUnauthorizedEntryPoint httpUnauthorizedEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http
+                .exceptionHandling().authenticationEntryPoint(httpUnauthorizedEntryPoint)
+                .and()
+                .headers().frameOptions().disable()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/**").permitAll()
+                .anyRequest().authenticated();
+
+    }
+
+}
---
+++ src/main/java/com/academy/carrental/security/dto/UnauthorizedResponseDTO.java
@@ -0,0 +1,13 @@
+package com.academy.carrental.security.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UnauthorizedResponseDTO {
+
+    private final String customMessage;
+    private final String exceptionMessage;
+
+}
